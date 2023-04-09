9e0079fcb7cab20ffb00853f74ac31b86e2aa0f8
HectorOrlando <14orgui@gmail.com>
2023-04-09 21:09:18 +0200

    rename file

--- src/Infrastructure/Controllers/ProductContoller.php
+++ src/Infrastructure/Controllers/ProductContoller.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace Infrastructure\Controllers;
 
 use Infrastructure\Repository\PDOProductRepository;
-use Application\ReadAllProducts;
+use Aplication\ReadAllProducts;
 
 require_once("/xampp/htdocs/covers-for-cell/src/Infrastructure/Repositories/PDOProductRepository.php");
-require_once("/xampp/htdocs/covers-for-cell/src/Application/ReadAllProducts.php");
+require_once("/xampp/htdocs/covers-for-cell/src/Aplication/ReadAllProducts.php");
 
 class ProductContoller
 {
